ecc96b232c76a3d521277bf708c9ecac301abb1b
Felix Stegmaier <stegmaier.felix@gmail.com>
2017-07-29 12:20:49 +0200

    add c syle

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,4 +5,4 @@ set -e
 test -x $STRIP
 $STRIP --shell-style "test/shell_test.sh" | diff "test/shell_test.expected.sh" -
 $STRIP --xml-style "test/xml_test.xml" | diff "test/xml_test.expected.xml" -
-#$STRIP --c-style "test/c_test.c" | diff "test/c_test.expected.c" -
+$STRIP --c-style "test/c_test.c" | diff "test/c_test.expected.c" -
